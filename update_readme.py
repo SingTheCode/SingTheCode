8231689fe602ed5c9440e432f5960615de027ec8
singco <65025333+SingTheCode@users.noreply.github.com>
2022-09-02 23:28:39 +0900

    Create update_readme.py

---
+++ update_readme.py
@@ -0,0 +1,53 @@
+import feedparser
+
+singco_velog_rss_url = "https://api.velog.io/rss/@singco"
+rss_feed = feedparser.parse(singco_velog_rss_url)
+
+MAX_POST_NUM = 5
+
+latest_blog_post_list = ""
+
+for idx, feed in enumerate(rss_feed['entries']):
+    if idx > MAX_POST_NUM:
+        break
+    feed_date = feed['published_parsed']
+    latest_blog_post_list += f"[{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
+
+markdown_text = """<h3 align="center">Welcome to Jaehyun Park's profile!<img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="40">
+<p align="center">
+<p align="center"><a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&pause=1000&color=9F88F7&width=450&lines=Frontend+developer+seeking+clear+code;Developer+leading+collaboration" alt="Typing SVG" /></a></p>
+<p align="center">
+  <a href="https://151958.notion.site/Singco-4ec518e8892149b0a9b82010c6d16741"><img src="https://img.shields.io/badge/Portfolio-000000?&style=for-the-badge&logo=notion&logoColor=white" height=25></a><a href="https://velog.io/@singco"><img src="https://img.shields.io/badge/Blog-20C997?&style=for-the-badge&logo=velog&logoColor=white" height=25></a><a href="https://www.instagram.com/singcotive/?hl=ko"><img src="https://img.shields.io/badge/Instagram-E4405F?&style=for-the-badge&logo=instagram&logoColor=white" height=25></a>
+</p>
+
+## 📚 Top open source projects
+<p align="left">
+  <a href="https://github.com/Collusic/collusic-new"><img width="400" src="https://denvercoder1-github-readme-stats.vercel.app/api/pin/?username=Collusic&repo=collusic-new&theme=react&hide_border=true&icon_color=F8D866&show_icons=false" alt="collusic-new"></a>
+  <a href="https://github.com/SingTheCode/fitween"><img width="400" src="https://denvercoder1-github-readme-stats.vercel.app/api/pin/?username=SingTheCode&repo=fitween&theme=react&hide_border=true&icon_color=F8D866&show_icons=false" alt="fitween"></a>
+  <a href="https://github.com/Collusic/collusic"><img width="400" src="https://denvercoder1-github-readme-stats.vercel.app/api/pin/?username=Collusic&repo=collusic&theme=react&hide_border=true&icon_color=F8D866&show_icons=false" alt="collusic"></a>
+  <a href="https://github.com/JS-GreenTea/web-with-js"><img width="400" src="https://denvercoder1-github-readme-stats.vercel.app/api/pin/?username=JS-GreenTea&repo=web-with-js&theme=react&hide_border=true&icon_color=F8D866&show_icons=false" alt="web-with-js"></a>
+  <a href="https://github.com/SingTheCode/POP-Movie"><img width="400" src="https://denvercoder1-github-readme-stats.vercel.app/api/pin/?username=SingTheCode&repo=POP-Movie&theme=react&hide_border=true&icon_color=F8D866&show_icons=false" alt="POP-Movie"></a>
+</p>
+
+## 🔥 Stats
+<p align="left">
+<a href="s">
+  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=SingTheCode&layout=compact&theme=react" width="350" />
+</a>
+<a href="s">
+  <img src="https://github-readme-stats.vercel.app/api?username=SingTheCode&theme=react&show_icons=true"  width="420"/>
+</a>  
+</p>
+
+## Skills
+<p align="left">
+  <img src="https://img.shields.io/badge/HTML5-E34F26?&style=for-the-badge&logo=HTML5&logoColor=white" height=25><img src="https://img.shields.io/badge/CSS3-1572B6?&style=for-the-badge&logo=CSS3&logoColor=white" height=25><img src="https://img.shields.io/badge/Sass-CC6699?&style=for-the-badge&logo=Sass&logoColor=white" height=25><img src="https://img.shields.io/badge/styled components-DB7093?&style=for-the-badge&logo=styled-components&logoColor=white" height=25><img src="https://img.shields.io/badge/JavaScript-F7DF1E?&style=for-the-badge&logo=JavaScript&logoColor=white" height=25><img src="https://img.shields.io/badge/TypeScript-3178C6?&style=for-the-badge&logo=TypeScript&logoColor=white" height=25><img src="https://img.shields.io/badge/React-61DAFB?&style=for-the-badge&logo=React&logoColor=white" height=25><img src="https://img.shields.io/badge/Vue.js-4FC08D?&style=for-the-badge&logo=Vue.js&logoColor=white" height=25><img src="https://img.shields.io/badge/Nginx-009639?&style=for-the-badge&logo=NGINX&logoColor=white" height=25><img src="https://img.shields.io/badge/AWS-232F3E?&style=for-the-badge&logo=Amazon AWS&logoColor=white" height=25><img src="https://img.shields.io/badge/GitHub-181717?&style=for-the-badge&logo=GitHub&logoColor=white" height=25><img src="https://img.shields.io/badge/GitLab-FC6D26?&style=for-the-badge&logo=GitLab&logoColor=white" height=25><img src="https://img.shields.io/badge/Jira-0052CC?&style=for-the-badge&logo=Jira Software&logoColor=white" height=25>
+</p>
+
+## Recent Blog
+"""
+
+readme_text = f"{markdown_text}{latest_blog_post_list}"
+
+with open("README.md", 'w', encoding='utf-8') as f:
+    f.write(readme_text)
